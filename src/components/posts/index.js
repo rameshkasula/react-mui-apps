04ec9982ac207da2a87407c75ed9dacf7b4ade6d
JSDEV1999 <rameshlearningadvance@gmail.com>
2022-05-06 23:08:48 +0530

    post card added

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,17 +1,19 @@
 import { Container, Grid } from "@mui/material";
 import React from "react";
-import Layout from "../Layout";
 import PostCard from "./PostCard";
 
 export default function Posts() {
   return (
     <Container maxWidth="md">
       <Grid container spacing={2}>
-        {Array.from(Array(6)).map((_, index) => (
-          <Grid item xs={12} sm={12} md={12} key={index}>
+        {Array.from(Array(12)).map((_, index) => (
+          <Grid item xs={12} sm={12} md={6} key={index}>
             <PostCard />
           </Grid>
         ))}
+        <Grid item xs={12} sm={12} md={6}>
+          <PostCard />
+        </Grid>
       </Grid>
     </Container>
   );
